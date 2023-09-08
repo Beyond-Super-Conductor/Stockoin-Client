971724c95b8050a1f38cb77b2b18804911f6039a
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-09-08 17:48:41 +0900

    fix: 전역 상태 저장 로직 수정

--- app/(tokens)/dashboard/[slug]/[tokenDetail]/page.tsx
+++ app/(tokens)/dashboard/[slug]/[tokenDetail]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { selectTokens } from '@/store/findToken';
+import { findCategoryState, selectTokens } from '@/store/findToken';
 import { selectTokenState } from '@/store/selectToken';
 import { tokenCategory } from '@/utils/constants';
 import { useParams } from 'next/navigation'
@@ -8,14 +8,20 @@ import { useRecoilState, useRecoilValue } from 'recoil'
 
 export default function page() {
   const [selectToken,setSelectToken] = useRecoilState(selectTokenState);
+  const [findCategory,setFindCategory] = useRecoilState(findCategoryState)
   const {slug, tokenDetail} = useParams();
   
   
   useEffect(() => {
     
-    const selectedToken = tokenCategory
-    .find((item) => item.enName === slug)
-    ?.tokens.find((item) => item.enName === tokenDetail);
+    const selectedCategory = tokenCategory.find((item) => item.enName === slug);
+    
+    if(!selectedCategory) return;
+    setFindCategory(selectedCategory);
+    
+    const selectedToken = selectedCategory.tokens
+    .find((item) => item.enName === tokenDetail);
+
     if(!selectedToken) return;
     setSelectToken(selectedToken);
 
