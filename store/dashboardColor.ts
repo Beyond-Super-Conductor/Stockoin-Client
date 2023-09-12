a3d5afc23be553059da2b2becf7376f2181b3a63
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-09-12 15:04:12 +0900

    chore: 기본 네비게이션 색상 변경

--- store/dashboardColor.ts
+++ store/dashboardColor.ts
@@ -8,5 +8,5 @@ type DashboardColor = string[]
 export const dashboardColorState = atom<DashboardColor>({
   key: 'dashboardColorState',
   
-  default: ['#5959a4', '#b3f864']
+  default: ['#5959a4', '#b3f864','#a1e05a','#5959a4']
 })
