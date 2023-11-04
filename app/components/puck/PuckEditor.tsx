c964d3f4e9c0f860ea5dcbcd9acb90f60f9d4b8d
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-11-05 01:10:32 +0900

    feat: puck radio width 변경

--- app/components/puck/PuckEditor.tsx
+++ app/components/puck/PuckEditor.tsx
@@ -1,19 +1,76 @@
 'use client'
 
 import React, { ReactNode } from 'react'
-import { Config, Puck, AppState, Data, DefaultComponentProps, DefaultRootProps,  } from '@measured/puck'
+import { Config, Puck, AppState, Data, DefaultComponentProps, DefaultRootProps } from '@measured/puck'
 import "@measured/puck/dist/index.css";
-
-interface Props {
-  HeadingBlock: { title: string };
-  Paragraph: {text: string}
-}
 
 
 const config:Config = {
   
   components: {
-    
+    ChatRoom: {
+      fields: {
+        title: {
+          type: 'radio',
+          options: [
+            // bitcoin categories
+            {label: '비트코인', value: 'BTC',},
+            {label: '이더리움', value: 'ETH'},
+            {label: '도지코인', value: 'DOGE'},
+            {label: '에이다', value: 'ADA'},
+            {label: '리플', value: 'XRP'},
+            {label: '폴카닷', value: 'DOT'},
+            {label: '유니스왑', value: 'UNI'},
+            {label: '테조스', value: 'XTZ'},
+            {label: '비체인', value: 'VET'},
+            {label: '라이트코인', value: 'LTC'},
+            {label: '체인링크', value: 'LINK'},
+            {label: '스텔라루멘', value: 'XLM'},
+            {label: '비트코인캐시', value: 'BCH'},
+            {label: '쎄타토큰', value: 'THETA'},
+            {label: '트론', value: 'TRX'},
+          ]
+        },
+        textAlign: {
+          type: "radio",
+          options: [
+            { label: "Left", value: "left" },
+            { label: "Center", value: "center" },
+            { label: "Right", value: "right" },
+          ],
+        },
+        textSize: {
+          type: "radio",
+          options: [
+            { label: "Small", value: "text-3xl" },
+            { label: "Normal", value: "text-4xl" },
+            { label: "Large", value: "text-5xl" },
+          ],
+        },
+        color: {
+          type: "radio",
+          options: [
+            { label: "빨강", value: "text-red-400" },
+            { label: "파랑", value: "text-indigo-400" },
+          ]
+        },
+      },
+      defaultProps: {
+        title: 'BTC',
+      },
+      render: ({ title, textAlign,color, textSize}) => (
+        <div style={{ padding: 64 }}>
+          <h1 style={{ textAlign }} className={`${textSize} ${color} mb-10` }>{title}</h1>
+          <div className={`
+          mx-auto w-[600px] h-[360px] border border-slate-200
+          flex flex-col justify-center items-center
+          `}>
+            <span className='text-slate-400'>여기에 채팅 들어갈건데요?</span>
+              
+          </div>
+        </div>
+      ),
+    },
     HeadingBlock: {
       fields: {
         title: {
@@ -67,9 +124,9 @@ const config:Config = {
         textSize: {
           type: "radio",
           options: [
-            { label: "Small", value: "text-sm" },
-            { label: "Normal", value: "text-base" },
-            { label: "Large", value: "text-lg" },
+            { label: "Small", value: "text-3xl" },
+            { label: "Normal", value: "text-4xl" },
+            { label: "Large", value: "text-5xl" },
           ],
         },
         color: {
@@ -87,7 +144,7 @@ const config:Config = {
       },
       render: ({ text,textAlign, color, textSize}) => (
         <div style={{ padding: 32 }}>
-          <p style={{textAlign}} className={`leading-7 text-4xl ${textSize} ${color}`}>{text}</p>
+          <p style={{textAlign}} className={`leading-7 ${textSize} ${color}`}>{text}</p>
         </div>
       ),
     },
@@ -103,6 +160,14 @@ const initialData:Data<DefaultComponentProps, DefaultRootProps> = {
         type: "Paragraph",
         children: [],
       },
+      {
+        type: "ChatRoom",
+        children: [],
+      },
+      {
+        type: "Paragraph",
+        children: [],
+      }
     ],
   },
   content: [
@@ -118,10 +183,11 @@ const save = (data: any) => {
 
 const myPlugin = {
   renderRootFields: ({ children, dispatch, state }:{children:ReactNode, dispatch:any, state:AppState}) => (
-    <div>
+    <div className='relative'>
       {children}
 
       <button
+        className='ml-6 px-2 w-fit h-10 bg-indigo-400 text-white rounded-lg'
         onClick={() => {
           dispatch({
             type: "setUi",
@@ -129,17 +195,12 @@ const myPlugin = {
           });
         }}
       >
-        Toggle side-bar
+        왼쪽 사이드바 {state.ui.leftSideBarVisible ? "숨기기" : "보이기"}
       </button>
     </div>
   ),
-  
 };
 
 export default function PuckEditor() {
-  return (
-    <div className='w-full mt-100 pt-[300px]'>
-      <Puck config={config} data={initialData} plugins={[myPlugin]} onPublish={save} />
-    </div>
-  )
+  return <Puck config={config} data={initialData} plugins={[myPlugin]} onPublish={save} />
 }
--- app/pan/page.tsx
+++ app/pan/page.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import GlobalHeader from '../nav'
 import PuckEditor from '../components/puck/PuckEditor'
+
+
 
 export default function PanPage() {
   return (
-    <>
+    <div>
     
-    <div className='mt-100'>마이판
+
     <PuckEditor />
+    
     </div>
-    </>
     
   )
 }
